ebe7ee5d4ae5c2ecc3374d49f6a92b62d41a3af4
Hugo Marotta <humtta@proton.me>
2026-06-25 10:29:45 -0300

    Set extension name and description

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,4 +4,9 @@ export default defineConfig({
   outDir: "out",
   srcDir: "src",
   entrypointsDir: "entries",
+
+  manifest: {
+    name: "Lyrics Presenter",
+    description: "Presents song lyrics in fullscreen.",
+  },
 });
