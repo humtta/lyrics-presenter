3c070a2578dfed5eb1f1679308f1817e0981080f
Hugo Marotta <humtta@proton.me>
2026-06-25 13:56:46 -0300

    Set extension icon path

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
 
   modules: ["@wxt-dev/auto-icons"],
 
+  autoIcons: {
+    baseIconPath: "assets/icon.svg",
+  },
+
   manifest: {
     name: "Lyrics Presenter",
     description: "Presents song lyrics in fullscreen.",
