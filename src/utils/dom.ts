beb588991c7a6ed6bb7a70b1d96bdaa8398064d1
Hugo Marotta <humtta@proton.me>
2026-06-27 17:25:29 -0300

    Add $$ function
    $$ returns all elements that match a CSS selector.

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -2,3 +2,8 @@ export const $ = <T extends HTMLElement>(
   selector: string,
   parent: ParentNode = document,
 ): T => parent.querySelector<T>(selector) as T;
+
+export const $$ = <T extends HTMLElement>(
+  selector: string,
+  parent: ParentNode = document,
+): NodeListOf<T> => parent.querySelectorAll<T>(selector);
