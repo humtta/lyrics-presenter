3b9c835340c3a9da38c6894dca04fe32a77aa985
Hugo Marotta <humtta@proton.me>
2026-06-27 00:54:31 -0300

    Add letras scraper selectors

--- src/scrapers/letras.ts
+++ src/scrapers/letras.ts
@@ -1,6 +1,12 @@
 import { Song } from "@/types";
 import { SongScraper } from "./scrapers";
 
+enum Selector {
+  Title = "#js-lyric-content h1",
+  Artist = "#js-lyric-content h2",
+  Lyrics = "#js-lyric-content .lyric-original",
+}
+
 export const letras: SongScraper = {
   matches: ["*://*.letras.com/*", "*://*.letras.mus.br/*"],
 
