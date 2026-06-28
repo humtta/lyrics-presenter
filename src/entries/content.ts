d3248a61318950a3d7faf6befc37b5981c61f32d
Hugo Marotta <humtta@proton.me>
2026-06-28 11:58:54 -0300

    Implement content script main function

--- src/entries/content.ts
+++ src/entries/content.ts
@@ -1,7 +1,28 @@
-import { getMatches } from "@/scrapers/scrapers";
+import { startPresentation } from "@/presentation/presentation";
+import { findScraper, getMatches } from "@/scrapers/scrapers";
+import { Message, type Song } from "@/types";
 
 export default defineContentScript({
   matches: getMatches(),
 
-  main() {},
+  main() {
+    let song: Song | null = null;
+
+    const scraper = findScraper(window.location.href);
+    if (scraper) {
+      song = scraper.scrape(document);
+    }
+
+    browser.runtime.onMessage.addListener((message, _, sendResponse) => {
+      switch (message.type) {
+        case Message.GetSongMeta:
+          sendResponse(song?.meta);
+          break;
+
+        case Message.StartPresentation:
+          startPresentation(song);
+          break;
+      }
+    });
+  },
 });
