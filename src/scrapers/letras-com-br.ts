7b0c4f802737d5c61ab2433b2a79e81379901f6b
Hugo Marotta <humtta@proton.me>
2026-06-28 17:54:56 -0300

    Set letrasComBr scraper matches

--- src/scrapers/letras-com-br.ts
+++ src/scrapers/letras-com-br.ts
@@ -2,7 +2,7 @@ import type { Song } from "@/types";
 import type { SongScraper } from "./scrapers";
 
 export const letrasComBr: SongScraper = {
-  matches: [],
+  matches: ["*://*.letras.com.br/*"],
 
   scrape(doc: Document): Song | null {
     return null;
