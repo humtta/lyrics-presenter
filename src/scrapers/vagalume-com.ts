0c96376f2c4d2066638c97e9b6b3662eb5e6ade2
Hugo Marotta <humtta@proton.me>
2026-06-29 17:36:49 -0300

    Add vagalumeCom scraper selectors

--- src/scrapers/vagalume-com.ts
+++ src/scrapers/vagalume-com.ts
@@ -1,6 +1,12 @@
 import type { Song } from "@/types";
 import type { SongScraper } from "./scrapers";
 
+enum Selector {
+  Title = "#lyricContent h1",
+  Artist = "#lyricContent h2",
+  Lyrics = "#lyricContent #lyrics",
+}
+
 export const vagalumeCom: SongScraper = {
   matches: ["*://*.vagalume.com/*", "*://*.vagalume.com.br/*"],
 
