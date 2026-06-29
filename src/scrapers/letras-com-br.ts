6696c14136576b9849ef3d201480a252c17b1dc3
Hugo Marotta <humtta@proton.me>
2026-06-28 22:25:44 -0300

    Add letrasComBr scraper selectors

--- src/scrapers/letras-com-br.ts
+++ src/scrapers/letras-com-br.ts
@@ -1,6 +1,12 @@
 import type { Song } from "@/types";
 import type { SongScraper } from "./scrapers";
 
+enum Selector {
+  Title = "#page-content .content-title-section h1",
+  Artist = "#page-content .content-title-section h2",
+  Lyrics = "#page-content .lyrics-section",
+}
+
 export const letrasComBr: SongScraper = {
   matches: ["*://*.letras.com.br/*"],
 
