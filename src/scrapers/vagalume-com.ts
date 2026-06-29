ae090c4d2fd5b747a1f54a84c48ec1b485fa3c14
Hugo Marotta <humtta@proton.me>
2026-06-29 13:58:17 -0300

    Set vagalumeCom scraper matches

--- src/scrapers/vagalume-com.ts
+++ src/scrapers/vagalume-com.ts
@@ -2,7 +2,7 @@ import type { Song } from "@/types";
 import type { SongScraper } from "./scrapers";
 
 export const vagalumeCom: SongScraper = {
-  matches: [],
+  matches: ["*://*.vagalume.com/*", "*://*.vagalume.com.br/*"],
 
   scrape(doc: Document): Song | null {
     return null;
